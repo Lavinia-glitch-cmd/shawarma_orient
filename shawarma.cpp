a62feec7f32d5b5961f083cfd5439e8e15be6119
Cartsea Lavinia <laviniac7355@gmail.com>
2026-03-11 16:43:56 +0200

    fixed main(made object ingredients)

--- shawarma.cpp
+++ shawarma.cpp
@@ -5,24 +5,44 @@ class ingredients
 {
     private:
 
+
     public:
         void open_menu();
 };
 void ingredients::open_menu()
 {
-    cout<<"==Choose ingredient==";
-    cout<<"1. meat";
-    cout<<"2. sauce";
-    cout<<"3. tomaotes";
-    cout<<"4. french fries";
+    int suboption=-1;
+    while(suboption!=0)
+    {
+        cout<<"==Choose ingredient==\n";
+        cout<<"1. meat\n";
+        cout<<"2. sauce\n";
+        cout<<"3. tomaotes\n";
+        cout<<"4. french fries\n";
+        cout<<"0. exit\n";
+        cin>>suboption;
+    }
+    /*switch (suboption)
+    {
+    case 1:
+        cout<<"choose one from the following: ";
+        cout<<"1. chicken";
+        cout<<"2. beef";
+        cout<<"3. lamb";
+        break;
+    
+    default:
+        break;
+    }*/
 }
 
 int main()
 {
     int option;
-    cout<<"== hello!! choose from the following ==";
-    cout<<"1. add ingredient ";
-    cout<<"0. Iesire";
+    ingredients ingredients;
+    cout<<"== hello!! choose from the following ==\n";
+    cout<<"1. add ingredient\n";
+    cout<<"0. exit\n";
 
     cin>>option;
     switch(option)
