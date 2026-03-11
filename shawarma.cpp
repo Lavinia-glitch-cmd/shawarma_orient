e1ecd1bc6bf4a12b76f12a11e104914ed6d93fe4
Cartsea Lavinia <laviniac7355@gmail.com>
2026-03-11 16:25:09 +0200

    added class ingredients and submenu

--- shawarma.cpp
+++ shawarma.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class ingredients
+{
+    private:
+
+    public:
+        void open_menu();
+};
+void ingredients::open_menu()
+{
+    cout<<"==Choose ingredient==";
+    cout<<"1. meat";
+    cout<<"2. sauce";
+    cout<<"3. tomaotes";
+    cout<<"4. french fries";
+}
+
 int main()
 {
+    int option;
     cout<<"== hello!! choose from the following ==";
     cout<<"1. add ingredient ";
     cout<<"0. Iesire";
+
+    cin>>option;
+    switch(option)
+    {
+        case 1:
+            ingredients.open_menu();
+    }
 }
